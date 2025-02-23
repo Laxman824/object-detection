4d056f30806918abe0655d40d2037210c29d3200
K Laxman <71651991+Laxman824@users.noreply.github.com>
2025-02-23 15:31:31 +0530

    v2

--- setup.sh
+++ setup.sh
@@ -1,18 +1,28 @@
-4. Create `setup.sh` script:
-```bash
+# 4. Create `setup.sh` script:
+# ```bash
+# #!/bin/bash
+
+# # Create required directories
+
+
+# # Make script executable
+# chmod +x setup.sh
 #!/bin/bash
 
-# Create required directories
+# Create the ~/.streamlit directory
+mkdir -p ~/.streamlit/
+
+# Configure Streamlit
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+# Download model during deployment
 mkdir -p tmp/model/rfcn/1
-
-# Download and extract model if not exists
-if [ ! -f tmp/model/rfcn/1/saved_model.pb ]; then
-    echo "Downloading RFCN model..."
-    wget https://storage.googleapis.com/intel-optimized-tensorflow/models/v1_8/rfcn_resnet101_fp32_coco_pretrained_model.tar.gz
-    tar -xzvf rfcn_resnet101_fp32_coco_pretrained_model.tar.gz -C tmp
-    mv tmp/rfcn_resnet101_coco_2018_01_28/saved_model/saved_model.pb tmp/model/rfcn/1/
-    rm -rf rfcn_resnet101_fp32_coco_pretrained_model.tar.gz tmp/rfcn_resnet101_coco_2018_01_28
-fi
-
-# Make script executable
-chmod +x setup.sh
+wget https://storage.googleapis.com/intel-optimized-tensorflow/models/v1_8/rfcn_resnet101_fp32_coco_pretrained_model.tar.gz
+tar -xzvf rfcn_resnet101_fp32_coco_pretrained_model.tar.gz -C tmp
+mv tmp/rfcn_resnet101_coco_2018_01_28/saved_model/saved_model.pb tmp/model/rfcn/1/
+rm -rf rfcn_resnet101_fp32_coco_pretrained_model.tar.gz tmp/rfcn_resnet101_coco_2018_01_28
